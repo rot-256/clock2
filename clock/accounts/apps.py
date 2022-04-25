9f1666120d44cde083b8d5068479373aff6c712f
rot-256 <divergence128@gmail.com>
2022-04-25 23:32:23 +0900

    Revert "プロジェクト ファイルを追加します。"
    This reverts commit ba88997dceedf496710db59eddfdec647f347998.

--- clock/accounts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class accountsConfig(AppConfig):
-    name = 'accounts'
--- clock/presence/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class presenceConfig(AppConfig):
-    name = 'presence'
